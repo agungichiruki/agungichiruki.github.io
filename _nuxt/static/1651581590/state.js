1f82b068330f6c0668cf9bd0336219ba77d427ea
agungichiruki <agung.ichiruki@gmail.com>
2022-05-03 20:41:54 +0800

    try to fix some motherfucker bug

---
+++ _nuxt/static/1651581590/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651581590",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"62f7416e"}}}}(null,"\u002F"));
