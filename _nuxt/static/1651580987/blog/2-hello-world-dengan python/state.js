b4b0baddb5131b5af37ea4cee345df37b6a43a8e
agungichiruki <agung.ichiruki@gmail.com>
2022-05-03 20:32:45 +0800

    initialize new blog

---
+++ _nuxt/static/1651580987/blog/2-hello-world-dengan python/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651580987",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F2-hello-world-dengan%20python",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0a708042"}}}}(null));
